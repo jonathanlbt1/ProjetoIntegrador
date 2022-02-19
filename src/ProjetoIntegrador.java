679c1c05fddd14fa829c82fa3a85f03ee13e9c06
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-02-18 21:31:06 -0300

    alterada classe principal

--- src/ProjetoIntegrador.java
+++ src/ProjetoIntegrador.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PTIAlgoritimos {
+public class ProjetoIntegrador {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
